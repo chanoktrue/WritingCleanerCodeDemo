579034218b51dd4da425e7cf26ed05152b1c8e82
chanoktrue <chanoktrue@me.com>
2024-04-03 05:57:47 +0700

    first proejct

---
+++ WritingCleanerCodeDemo/WritingCleanerCodeDemo/WritingCleanerCodeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WritingCleanerCodeDemoApp.swift
+//  WritingCleanerCodeDemo
+//
+//  Created by Thongchai Subsaidee on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct WritingCleanerCodeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
